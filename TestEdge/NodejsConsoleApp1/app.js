8a981426a2209ed4ce8589811199cd9b38f1c92c
Javier Goday <jgoday@gmail.com>
2016-03-12 19:29:51 +0100

    Importacion inicial

---
+++ TestEdge/NodejsConsoleApp1/app.js
@@ -0,0 +1,26 @@
+﻿var edge = require("edge");
+
+var create_person = edge.func({
+	assemblyFile: "../TestEdje/bin/Debug/TestEdje.exe",
+	typeName: 'TestEdje.Startup',
+	methodName: "CreatePerson"
+});
+
+var debug_error = function (error) {
+	console.log("Error: " + error);
+}
+
+var on_create_person = function (error, result) {
+	if (error) {
+		debug_error(error);
+	}
+	else {
+		console.log(result.Name);
+	}
+	
+	console.timeEnd("create_person")
+}
+
+console.time("create_person");
+create_person("Javier", on_create_person);
+create_person("Javier", on_create_person);
---
+++ TestEdge/TestEdge/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestEdge
+{
+    public class Startup
+    {
+        private int _count = 0;
+
+        public async Task<object> CreatePerson(string input)
+        {
+            var p = new Person(input);
+            _count++;
+
+            return await Task.FromResult(p);
+        }
+    }
+}
